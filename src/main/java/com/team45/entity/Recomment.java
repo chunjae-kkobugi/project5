2e4804601bcdd181437ea8180256bd30de27132a
sendjin5 <139240093+sendjin5@users.noreply.github.com>
2023-11-18 19:55:58 +0900

    [✨ ] recomment 정리중 (#20)
    * login
    
    * 회원가입 진행중
    
    * 휴면계정 진행중
    
    * 휴면계정 완료
    
    * Delete src/main/java/com/team45/mapper/JpaRepository.java
    
    * "notice"추가, 자주쓰는 답변 추가 , 오른쪽 상단 위 알림창 추가(헤더에 있음)
    
    * 잠시 커밋
    
    * up
    
    ---------
    
    Co-authored-by: SAM <sangmin990816@gmail.com>

--- src/main/java/com/team45/entity/Recomment.java
+++ src/main/java/com/team45/entity/Recomment.java
@@ -1,10 +1,14 @@
 package com.team45.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Recomment {
     private int no;
-    private int id_no;
+    private String mem_id;
     private String comment;
 }
