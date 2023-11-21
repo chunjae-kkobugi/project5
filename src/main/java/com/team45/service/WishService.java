4f6fcbddc7eb01ba585bb261fb20afc9765473dd
hansun321 <62285512+hansun321@users.noreply.github.com>
2023-11-21 19:13:12 +0900

    [🎨] index 완료 (#32)
    * login
    
    * 회원가입 진행중
    
    * 휴면계정 진행중
    
    * [:sparkles:] 마이페이지 상품
    
    * [:recycle:] url 수정
    
    * [:sparkles:] 내 상품 페이징
    
    * [:truck:] resources 수정
    
    * [:truck:] css 디렉토리 추가
    
    * [:sparkles:] 내 상품 가격 표시
    
    * [:art:] 프로필 사진 변경
    
    * [:sparkles:] 상품 전체 개수 표시
    
    * Delete log/2023-11/kimRestApiLog.2023-11-14.log
    
    * 리스트 구현 중
    
    * [:sparkles:] product list, detail
    
    * [:bug:] git 충돌 수정
    
    * [:recycle:] 사이드바 분리
    
    * [:recycle:] 마이페이지 판매 상품
    
    * [:art:] 마이페이지 키워드 추가
    
    * [:recycle:] script fragment 분리
    
    * [:recycle:] product insert의 fileupload 수정
    
    * [:recycle:] 클래스 이름 수정
    
    * [:sparkles:] 상품 찜
    
    * [:bug:] sql 수정
    
    * [:memo:] sql문 추가
    
    * [:bug:] 검색 옵션 수정
    
    * [:sparkles:] 마이페이지 찜 목록
    
    * [:truck:] myshop 이동
    
    * [:sparkles:] 키워드 등록
    
    * [:recycle:] 코드 개선
    
    * [:sparkles:] 마이페이지에서 찜 제거
    
    * [:sparkles:] 마이페이지에서 상품 이미지 표시
    
    * [:recycle:] 코드 정리
    
    * [:sparkles:] 키워드 추가, 삭제
    
    * [:bug:] 비회원 상품 상세페이지 접속
    
    * [:art:] 마이페이지 사이드메뉴
    
    * [:recycle:] 충돌 해결
    
    * [:art:] index
    
    * [:art:] index에 상품 목록 추가
    
    * [:art:] header active 추가
    
    * [:art:] header active 제거
    
    ---------
    
    Co-authored-by: sendjin5 <sendjin5@gmail.com>
    Co-authored-by: SAM <sangmin990816@gmail.com>
    Co-authored-by: emssme <bk75121@gmail.com>

--- src/main/java/com/team45/service/WishService.java
+++ src/main/java/com/team45/service/WishService.java
@@ -1,7 +1,7 @@
 package com.team45.service;
 
-import com.team45.entity.ProductVO;
 import com.team45.entity.Wish;
+import com.team45.entity.WishProduct;
 import com.team45.util.Page;
 
 import java.util.List;
@@ -9,7 +9,9 @@ import java.util.List;
 public interface WishService {
     List<Wish> wishList(String uid);
 
-    List<ProductVO> wishProductList(String uid, Page page);
+    Wish wishGet(Long wno);
+
+    List<WishProduct> wishProductList(String uid, Page page);
 
     int wishProductCount(String uid, Page page);
 
@@ -24,6 +26,8 @@ public interface WishService {
     void increaseWish(Long pno);
 
     void decreaseWish(Long pno);
+
+    void wishRemove(Long wno);
 
     int checkWish(Wish wish);
 }
