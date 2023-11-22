b5fb713768371eafadf7dfaa39635b0dd7a000f5
sendjin5 <139240093+sendjin5@users.noreply.github.com>
2023-11-22 13:38:46 +0900

    Sendjin5 (#37)
    * login
    
    * 회원가입 진행중
    
    * 휴면계정 진행중
    
    * 휴면계정 완료
    
    * Delete src/main/java/com/team45/mapper/JpaRepository.java
    
    * "notice"추가, 자주쓰는 답변 추가 , 오른쪽 상단 위 알림창 추가(헤더에 있음)
    
    * 잠시 커밋
    
    * up
    
    * up
    
    * 일단 업로드
    
    * Delete log/2023-11/kimRestApiLog.2023-11-14.log
    
    * Delete log/kimRestApiLog.log
    
    * 재업로드
    
    * upload
    
    * upload
    
    * 업로드
    
    * 업로드
    
    ---------
    
    Co-authored-by: SAM <sangmin990816@gmail.com>

--- src/main/java/com/team45/ctrl/ProductCtrl.java
+++ src/main/java/com/team45/ctrl/ProductCtrl.java
@@ -9,9 +9,15 @@ import com.team45.service.ChatService;
 import com.team45.service.ProductService;
 import com.team45.service.WishService;
 import com.team45.util.Page;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -20,9 +26,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+@Slf4j
 @Controller
 @RequestMapping("/product/**")
 public class ProductCtrl {
@@ -32,6 +42,9 @@ public class ProductCtrl {
     private WishService wishService;
     @Autowired
     private ChatService chatService;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     // 전체 상품 리스트
     @GetMapping("list")
@@ -118,10 +131,14 @@ public class ProductCtrl {
     public String productInsertPro(Product product, @RequestParam("upfile") MultipartFile[] files, HttpServletRequest request, Model model, RedirectAttributes rttr) throws IOException {
         HttpSession session = request.getSession();
 
+        Resource resource = new ClassPathResource("/static/images");
+        String uploadDir = resource.getFile().getAbsolutePath();
+
+
         String realPath = "C://upload/";
 //        String realPath = "D:/kim/project/tproj/project05/team45/src/main/resources/static/images";
         String today = new SimpleDateFormat("yyMMdd").format(new Date());
-        String saveFolder = realPath + today;
+        String saveFolder = uploadDir + today;
         System.out.println(saveFolder);
 
         File folder = new File(saveFolder);
@@ -130,24 +147,43 @@ public class ProductCtrl {
         }
 
         List<FileData> fileDataList = new ArrayList<>();
-
+//        3030cd5b-59af-4ce2-8ddb-907bdf2c4fcb_4.jpg
         for (MultipartFile file : files) {
             FileData fileData = new FileData();
             String originalFileName = file.getOriginalFilename();   // 첨부파일의 실제 이름
 
+            // 업로드된 파일 이름 중복 방지를 위해 유니크한 파일 이름 생성
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            String uniqueFileName = System.currentTimeMillis() + "_" + fileName;
+
+            // 업로드 디렉토리 경로 생성
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+            logger.info("uploadPathㅡㅡㅡㅡㅡㅡ"+ uploadPath);
+
+            // 파일을 업로드 디렉토리로 저장
+            Path filePath = uploadPath.resolve(uniqueFileName);
+
             if (!file.isEmpty()) { // 파일이 비어있지 않은 경우에만 처리
+                File dest = filePath.toFile();
 
                 String saveFilename = UUID.randomUUID().toString() + "_" + originalFileName;
                 fileData.setTableName("product");
                 fileData.setColumnNo(product.getPno());
                 fileData.setOriginName(originalFileName);
-                fileData.setSaveName(saveFilename);
+//                fileData.setSaveName(saveFilename);
+                fileData.setSaveName(uniqueFileName);
                 fileData.setSavePath(today);
                 fileData.setFileType(" ");
                 fileData.setStatus("ACTIVE");
-                file.transferTo(new File(saveFolder, saveFilename)); // 파일을 업로드 폴더에 저장
+                file.transferTo(dest); // 파일을 업로드 폴더에 저장
+//                file.transferTo(new File(saveFolder, saveFilename); // 파일을 업로드 폴더에 저장
+                logger.info("upload fileㅡㅡㅡㅡㅡㅡ"+ uploadPath+today+saveFilename);
             }
             fileDataList.add(fileData);
+
         }
 
         product.setFileDataList(fileDataList);
